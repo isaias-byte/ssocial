03235aa74a620a8d4f317e2c5bdc59618689ee8d
Isaias Juarez Esparza <isaias.juarez6580@alumnos.udg.mx>
2021-09-03 17:36:53 -0500

    Implementamos tema en la página de inicio

---
+++ resources/views/inicio.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.windmill')
+@section('contenido')
+    <h2 class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200">
+        Inicio
+    </h2>       
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProgramaController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('inicio', function () {
+    return view('inicio');
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -20,3 +25,5 @@ Route::get('/', function () {
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+Route::resource('programa', ProgramaController::class);
